ecaddb834af3a1a50950bc8f2ade65d87ff6d33f
sheppardjm <jsheppard@neucadia.com>
2024-09-30 15:14:54 -0400

    Fix small nav

--- src/components/headers/IndexSectionHeaders1.js
+++ src/components/headers/IndexSectionHeaders1.js
@@ -16,6 +16,7 @@ export default function IndexSectionHeaders1() {
     }
   }, []);
   const [isPlaying, setIsPlaying] = React.useState(false);
+  const [navOpen, setNavOpen] = React.useState(false);
   const ref = React.useRef(null);
   const handleClick = () => {
     const nextIsPlaying = !isPlaying;
@@ -150,7 +151,11 @@ export default function IndexSectionHeaders1() {
                       <path d="M6.54 5c.06.89.21 1.76.45 2.59l-1.2 1.2c-.41-1.2-.67-2.47-.76-3.79h1.51m9.86 12.02c.85.24 1.72.39 2.6.45v1.49c-1.32-.09-2.59-.35-3.8-.75l1.2-1.19M7.5 3H4c-.55 0-1 .45-1 1 0 9.39 7.61 17 17 17 .55 0 1-.45 1-1v-3.49c0-.55-.45-1-1-1-1.24 0-2.45-.2-3.57-.57a.84.84 0 0 0-.31-.05c-.26 0-.51.1-.71.29l-2.2 2.2a15.149 15.149 0 0 1-6.59-6.59l2.2-2.2c.28-.28.36-.67.25-1.02A11.36 11.36 0 0 1 8.5 4c0-.55-.45-1-1-1z" />
                     </svg>
                   </a>
-                  <a href="#" className="xl:hidden">
+                  <a
+                    href="#"
+                    className="xl:hidden"
+                    onClick={() => setNavOpen((prevState) => !prevState)}
+                  >
                     <svg
                       className="navbar-burger text-white"
                       width={51}
@@ -176,7 +181,11 @@ export default function IndexSectionHeaders1() {
                   </a>
                 </div>
               </nav>
-              <div className="hidden fixed top-0 left-0 bottom-0 w-5/6 max-w-xs z-50">
+              <div
+                className={`top-0 left-0 bottom-0 w-5/6 max-w-xs z-50 ${
+                  navOpen ? `fixed` : `hidden`
+                }`}
+              >
                 <div className="fixed inset-0 bg-black opacity-20" />
                 <nav className="relative p-8 w-full h-full bg-white overflow-y-auto">
                   <div className="flex items-center justify-between">
@@ -187,7 +196,10 @@ export default function IndexSectionHeaders1() {
                         alt
                       />
                     </a>
-                    <a href="#">
+                    <a
+                      href="#"
+                      onClick={() => setNavOpen((prevState) => !prevState)}
+                    >
                       <svg
                         width={24}
                         height={24}
@@ -305,7 +317,7 @@ export default function IndexSectionHeaders1() {
                 </nav>
               </div>
             </div>
-            <div className="relative z-50 pt-80">
+            <div className="relative z-40 pt-80">
               <h1 className="font-heading tracking-tight text-5xl md:text-7xl text-white font-medium max-w-xs md:max-w-xl mb-6">
                 Welcome to Harvey's
               </h1>
